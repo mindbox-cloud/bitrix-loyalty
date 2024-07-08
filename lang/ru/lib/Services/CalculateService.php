83ce9024e8cfaa3318e1da6ad5e894e3a7c32791
Ilya Vyrezkov <i.vyrezkov@intensa.ru>
2024-07-08 10:18:02 +0300

    MBM-477 добавлены переводы для укр

---
+++ lang/ru/lib/Services/CalculateService.php
@@ -0,0 +1,4 @@
+<?php
+$MESS['MINDBOX_LOYALTY_COUPON_NOT_FOUND'] = 'Промокод не найден';
+$MESS['MINDBOX_LOYALTY_COUPON_CAN_NOT_BE_USER'] = 'Нельзя применить данный промокод';
+$MESS['MINDBOX_LOYALTY_COUPON_USED'] = 'Промокод был использован ранее';
