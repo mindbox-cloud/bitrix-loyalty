db5bd52d07eee9b4764ba8e1e8bde21f8e1c75e9
Ilya Vyrezkov <i.vyrezkov@intensa.ru>
2024-08-08 16:49:07 +0300

    MBM-538 калькулейтов промокодов и бонусов из свойства заказа

--- lib/Services/CalculateService.php
+++ lib/Services/CalculateService.php
@@ -250,7 +250,9 @@ class CalculateService
         /** @var CalculateUnauthorizedCart $calculateUnauthorizedCart */
         $calculateUnauthorizedCart = $this->serviceLocator->get('mindboxLoyalty.calculateUnauthorizedCart');
 
-        $mindboxOrder->setCoupons($this->sessionStorage->getPromocodeValue());
+        if ($this->sessionStorage->isPromocodeUsed()) {
+            $mindboxOrder->setCoupons($this->sessionStorage->getPromocodeValue());
+        }
 
         $orderData = $mindboxOrder->getData();
         unset($orderData['totalPrice']);
