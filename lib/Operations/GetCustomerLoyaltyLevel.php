a322e4c63ce62ce3b6ac37c66f56e3e8b51d1dd5
Ilya Vyrezkov <i.vyrezkov@intensa.ru>
2024-08-27 10:20:40 +0300

    MBM-555 операция для уровня лояльности

---
+++ lib/Operations/GetCustomerLoyaltyLevel.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mindbox\Loyalty\Operations;
+
+use Mindbox\DTO\DTO;
+use Mindbox\DTO\V3\Requests\CustomerRequestDTO;
+use Mindbox\Exceptions\MindboxClientException;
+use Mindbox\Loyalty\Exceptions\ErrorCallOperationException;
+use Mindbox\MindboxResponse;
+
+class GetCustomerLoyaltyLevel extends AbstractOperation
+{
+    /**
+     * @param CustomerRequestDTO $customerRequestDTO
+     * @param array<string> $segmentations
+     * @return MindboxResponse
+     * @throws ErrorCallOperationException
+     */
+    public function execute(CustomerRequestDTO $customerRequestDTO, array $segmentations): MindboxResponse
+    {
+        try {
+            $operation = $this->getOperation();
+            $client = $this->api();
+
+            $client->prepareRequest(
+                method: 'POST',
+                operationName: $operation,
+                body: new DTO(['customer' => $customerRequestDTO, 'segmentations' => $segmentations]),
+                addDeviceUUID: false
+            )->getRequest();
+
+            $response = $client->sendRequest();
+
+            return $response;
+        } catch (MindboxClientException $e) {
+            throw new ErrorCallOperationException(
+                message: sprintf('The operation %s failed', $this->getOperation()),
+                previous: $e,
+                operationName: $this->getOperation()
+            );
+        }
+    }
+
+    protected function operation(): string
+    {
+        return 'GetCustomerLoyaltyLevel';
+    }
+}
