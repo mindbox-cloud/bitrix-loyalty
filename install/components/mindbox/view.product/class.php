8204867f021e1faab532f2287c2acf9e836504b7
Михаил Селиванов <mixalin17@yandex.ru>
2024-09-13 14:46:14 +0300

    MBM-578 передача группы

--- install/components/mindbox/view.product/class.php
+++ install/components/mindbox/view.product/class.php
@@ -21,8 +21,12 @@ class ViewProduct extends CBitrixComponent
         $settings = \Mindbox\Loyalty\Support\SettingsFactory::create();
 
         $this->arResult['PRODUCT_ID'] = $this->arParams['PRODUCT_ID'];
+        $this->arResult['PRODUCT_GROUP_ID'] = $this->arParams['PRODUCT_GROUP_ID'];
+        $this->arResult['PRICE'] = $this->arParams['PRICE'];
         $this->arResult['OPERATION_PREFIX'] = $settings->getWebsitePrefix();
         $this->arResult['ID_KEY'] = $settings->getExternalProductId();
+
+        $this->arResult['PAYLOAD'] = $this->getPayload();
 
         $this->includeComponentTemplate();
     }
@@ -32,9 +36,45 @@ class ViewProduct extends CBitrixComponent
         return $arParams;
     }
 
+    public function getPayload()
+    {
+        $payload = [
+            'operation' => $this->arResult['OPERATION_PREFIX'] . '.ViewProduct',
+            'data' => [
+                'viewProduct' => [
+
+                ]
+            ]
+        ];
+
+        if ($this->arResult['PRODUCT_ID']) {
+            $payload['data']['viewProduct'] = [
+                'product' => [
+                    'ids' => [
+                        $this->arResult['ID_KEY'] => $this->arResult['PRODUCT_ID']
+                    ]
+                ]
+            ];
+        }
+
+        if ($this->arResult['PRODUCT_GROUP_ID']) {
+            $payload['data']['viewProduct'] = [
+                'productGroup' => [
+                    'ids' => [
+                        $this->arResult['ID_KEY'] => $this->arResult['PRODUCT_GROUP_ID']
+                    ]
+                ]
+            ];
+        }
+
+        if (isset($this->arResult['PRICE'])) {
+            $payload['data']['viewProduct']['price'] = $this->arResult['PRICE'];
+        }
+
+        return $payload;
+    }
     private function loadModule()
     {
-
         try {
             return Loader::includeModule('mindbox.loyalty');
         } catch (LoaderException $e) {
