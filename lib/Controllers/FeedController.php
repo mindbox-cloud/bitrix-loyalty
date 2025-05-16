75a94ad72356c052d9b00449adbdda011f39292a
Михаил Селиванов <mixalin17@yandex.ru>
2025-05-16 12:00:56 +0300

    fix error

--- lib/Controllers/FeedController.php
+++ lib/Controllers/FeedController.php
@@ -24,10 +24,39 @@ class FeedController extends \Bitrix\Main\Engine\Controller
 
     public function updateAction(string $siteId)
     {
+        $iterSites = \Bitrix\Main\SiteTable::getList([
+            'filter' => [],
+            'select' => ['LID'],
+        ]);
+
+        $isReturned = true;
+        foreach ($iterSites as $site) {
+            if ($site['LID'] === $siteId) {
+                $isReturned = false;
+            }
+        }
+
+        if ($isReturned) {
+            return ['message' => Loc::getMessage('FEED_IS_RUNNING')];
+        }
+
+        $iterator = \CAgent::GetList(
+            [],
+            [
+                'MODULE_ID' => 'mindbox.loyalty',
+                'NAME' => '\Mindbox\Loyalty\Feed\FeedGenerator::generate(\'' . $siteId . '%',
+            ]
+        );
+
+        if ($iterator->Fetch()) {
+            return ['message' => Loc::getMessage('FEED_IS_RUNNING')];
+        }
+
         $agent = \CAgent::AddAgent(
             "\Mindbox\Loyalty\Feed\FeedGenerator::generate('$siteId');",
-            'mindbox.loyalty',
+            'mindbox.loyalty'
         );
+
         if ($agent) {
             return ['message' => Loc::getMessage('FEED_IS_RUN')];
         }
